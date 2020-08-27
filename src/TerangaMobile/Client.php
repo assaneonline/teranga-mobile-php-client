cace22bde58ce3bd7cc87a8674b8ca291bd2078b
assane101 <assane101@gmail.com>
2020-08-27 10:08:55 +0000

    Added method Client::getSearchFieldUid

--- src/TerangaMobile/Client.php
+++ src/TerangaMobile/Client.php
@@ -6,6 +6,10 @@ class Client extends \Core\BaseClient {
 
     const API_BASE_PATH = 'http://teranga-mobile.pasteur.sn/api/v1/';
 
+    function getSearchFieldUid(){
+        return $_REQUEST["field_uid"] ?? FALSE;
+    }
+
     function getSearchFieldQuery(){
         return $_REQUEST["query"] ?? FALSE;
     }
