8208173f3a482008d48080a09a58edd7abb52c42
assane101 <assane101@gmail.com>
2020-08-27 10:33:53 +0000

    Added BaseClient::getJSONInputStream for non-standard post content types (like from Android OkHttp)

--- src/Core/BaseClient.php
+++ src/Core/BaseClient.php
@@ -7,6 +7,7 @@ class BaseClient {
 
     protected $config = [];
     protected $errorMessage = "";
+    protected $JSONInput = null;
 
     function __construct($config = []){
         $this->config = array_merge(
@@ -44,6 +45,13 @@ class BaseClient {
             return json_encode($response, true);
 
         return FALSE;
+    }
+    
+    function getJSONInputStream(){
+        if(is_null($this->JSONInput))
+            $this->JSONInput = json_decode(file_get_contents('php://input'), true);
+
+        return $this->JSONInput;
     }
 
     function getErrorMessage(){
